8c12e614e6ca94802b094869cb4665268ab1f56a
kvb <kvb@github.com>
2017-12-06 14:36:44 -0600

    started structuring index.html and started adding variables to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+// furniture options (categotry)
+	var furnitureOptions = ["desk", "couch", "bookshelf", "table", "chair"]
+// letter options
+
+// word currently guessing
+	var selectedFurOpt = "";
+
+// dashes for where the letters go
+	var numOflettrsInFurOpt = [];
+	var numOfBlanks = 0;
+
+// letters guessed correctly
+	var 
+
+// number of guesses left
+
+
+// number of wins
+
+
+// number of losses
+
+
+// display press a letter key to get started
+
+
+// display dashes
+
+
+// display letters guessed
+
+
+// display number of guesses left
+
+
+// display number of wins
+
+
+// display number of losses
+
+});
